58299b3c60cad774028a01de182666d74de7c303
belldy <drieliluz@gmail.com>
2019-05-03 11:25:00 +0200

    update

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,29 +1,22 @@
 class LikesController < ApplicationController
       before_action :authenticate_user, only: [:new, :create, :destroy]
-      before_action :find_gossip
-      before_action :find_like, only: [:destroy]
+     
 
   def new
   end
 
   def create
-    if already_liked?
-        flash[:notice] = "You can't like more than once"
-    else
-      @like = Like.create(user: current_user.id, gossip: params[:gossip_id])
-      flash[:notice] = "You liked this gossip!"
-    end
+    @like = Like.create(user: current_user.id, gossip: params[:gossip_id])
+    flash[:success] = "You liked this gossip!"
+  end
     redirect_to gossip_path(params[:gossip_id])
 
   end
 
   def destroy
-    if !(already_liked?)
-        flash[:notice] = "Cannot unlike"
-      else
-        @like.destroy
-        flash[:danger] = "Disliked!"
-      end
+    @like.destroy
+    flash[:danger] = "Disliked!"
+  end
     redirect_to gossip_path(params[:gossip_id])
   end
 
@@ -38,14 +31,6 @@ class LikesController < ApplicationController
         flash[:danger] = "Please log in to continue"
         redirect_to new_session_path
     end
-  end
-
-  def find_gossip
-      @gossip = Gossip.find(params[:gossip_id])
-  end
-
-  def find_like
-     @like = @gossip.likes.find(params[:id])
   end
 
 end
